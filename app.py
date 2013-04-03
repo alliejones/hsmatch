8df944ea7ee816d9095d1ce49a7a7090f72c58dc
Allie Jones <allie.jones@gmail.com>
2013-04-03 14:38:38 -0400

    Give up on JS-based scraping/update, refactor JS

--- app.py
+++ app.py
@@ -1,56 +1,30 @@
 import os, json
-from flask import Flask, Response, request
+from flask import Flask, request
 from hsmatch import HSMatch
-from werkzeug.contrib.cache import SimpleCache
 
 app = Flask(__name__)
 app.debug = True
-
-@app.before_first_request
-def before_first_request():
-  app.hsmcache = SimpleCache()
-  app.hsmcache.set('match_data', {})
 
 @app.route('/')
 def index():
   person = request.args.get('person', None)
   callback = request.args.get('callback', None)
-  content = ''
-
   if person is None:
-    content = json.dumps({})
+    return json.dumps({})
   else:
     data = match_json(person)
     if callback is None:
-      content = data
+      return data
     else:
-      content = callback + '(' + data + ')'
-
-  resp = Response('success', status=200, mimetype='application/javascript')
-  return resp
-
-@app.route('/update', methods = ['POST'])
-def update():
-  data = request.form['data']
-  if data is not None:
-    app.hsmcache.set('match_data', json.loads(data))
-
-  resp = Response('success', status=200, mimetype='text/plain')
-  resp.headers.add('Access-Control-Allow-Origin', '*')
-  return resp
-
+      return callback + '(' + data + ')'
 
 
 def match_json(person):
-  data = app.hsmcache.get('match-data')
-  if data is not None:
-    sim = HSMatch(data)
-    try:
-      results = sim.compare(person)[1:6]
-      return json.dumps(results)
-    except KeyError:
-      return json.dumps({})
-  else:
+  sim = HSMatch()
+  try:
+    results = sim.compare(person)[1:6]
+    return json.dumps(results)
+  except KeyError:
     return json.dumps({})
 
 
--- bookmarklet.js
+++ bookmarklet.js
@@ -1,86 +1,107 @@
-$(document).ready(function() { if (typeof HSMatch === 'undefined') {
-  var baseUrl = 'http://0.0.0.0:5000/';
+;(function() {
+  // don't reinitialize the bookmarklet if it has already been run
+  if(typeof HSMatch === 'undefined') {
 
-  /* Results display */
-
-  var modalStyles = {
-    background: '#fff',
-    border: '5px solid #000',
-    width: '920px',
-    height: '500px',
-    position: 'fixed',
-    top: '50%',
-    marginTop: '-250px',
-    left: '50%',
-    marginLeft: '-485px',
-    padding: '0 20px',
-    boxShadow: '0 0 15px rgba(0, 0, 0, .5)'
-  };
-
-  var showModal = function (name) {
-    var html = '<div id="hsmatch-modal"><h1>Hacker School Match</h1><br><ul class="matches" style="text-align:center"><p class="loading">Loading matches for '+name+' ...</p></ul><p><a href="#" class="close">Close</a></div>';
-    $(html).css(modalStyles).appendTo('body');
-    getMatches(name);
-  };
-
-  var closeModal = function (e) { $('#hsmatch-modal').remove(); e.preventDefault(); }
-
-  var findMatches = function (e) {
-    e.preventDefault();
-    var name = $(e.target).prev('.name').text();
-    showModal(name);
-  };
-
-  var createLinks = function() {
-    $('.person').css('height', '280px');
-    $('.name').filter(function() {
-      return $(this).siblings('.skills').text() !== '';
-    }).after('<a href="#" class="find_matches">Find matches</a>');
-
-  };
-
-
-  var getMatches = function (name) {
-    $.getJSON(baseUrl + "?person="+encodeURIComponent(name)+"&callback=?", null, showMatches(name));
-  };
-
-  var showMatches = function(name) {
-    var name = name;
-
-    return function (matches) {
-      var selector = $.map(matches, function(match, i) { return '.person:contains("'+match[0]+'")'; }).join(', ');
-      var people = $(selector).clone().css({ width: '150px', display: 'inline-block', verticalAlign: 'top', margin: '15px' });
-      $('.find_matches, .skills, .irc', people).remove();
-      $('#hsmatch-modal .matches').empty().append('<h2>Hacker Schoolers with the skillset most like '+name+':</h2>').after(people);
+    var HSMatch = function() {
+      this.baseUrl = 'http://0.0.0.0:5000/';
     };
-  };
+
+    HSMatch.prototype.init = function() {
+      this.modal = new Modal(this);
+      this.createLinks();
+
+      $('body').on('click', '.close', this.modal.closeHandler());
+      $('body').on('click', '.find_matches', this.showModalHandler());
+    };
+
+    HSMatch.prototype.createLinks = function() {
+      $('.person').css('height', '280px');
+      $('.name').filter(function() {
+        return $(this).siblings('.skills').text() !== '';
+      }).after('<a href="#" class="find_matches">Find matches</a>');
+    };
+
+    HSMatch.prototype.showModalHandler = function() {
+      var modal = this.modal;
+      return function(e) {
+        e.preventDefault();
+        var name = $(e.target).prev('.name').text();
+        modal.show(name);
+      }
+    };
+
+    HSMatch.prototype.showMatchesHandler = function(name) {
+      var self = this;
+      var name = name;
+
+      return function (matches) {
+        var selector = $.map(matches, function(match, i) { return '.person:contains("'+match[0]+'")'; }).join(', ');
+        var people = $(selector).clone().css({ width: '150px', display: 'inline-block', verticalAlign: 'top', margin: '15px' });
+        $('.find_matches, .skills, .irc', people).remove();
+        $('#'+self.modal.id+' .matches').empty().append('<h2>Hacker Schoolers with the skillset most like '+name+':</h2>').after(people);
+      };
+    };
+
+    HSMatch.prototype.getMatches = function (name) {
+      $.getJSON(this.baseUrl + "?person="+encodeURIComponent(name)+"&callback=?", null, this.showMatchesHandler(name));
+    };
 
 
-  /* Setup */
-
-  // Scrape the skill data from the page
-  var scrape = function() {
-    var people = [];
-    $.each($('.person'), function() {
-      var skills = $('.skills', this).text().split(', ');
-      $.map(skills, function (val, i) {
-        return $.trim(val).toLowerCase().replace(/[-\/]/g, '');
+    /* Not used in bookmarklet itself
+     * Returns person/skill data from page as an object
+     */
+    HSMatch.prototype.scrape = function() {
+      var people = [];
+      $.each($('.person'), function() {
+        var skills = $('.skills', this).text().split(', ');
+        $.map(skills, function (val, i) {
+          return $.trim(val).toLowerCase().replace(/[-\/]/g, '');
+        });
+        people.push({
+          name: $('.name', this).text(),
+          skills: skills
+        });
       });
-      people.push({
-        name: $('.name', this).text(),
-        skills: skills
-      });
-    });
-    return people;
-  };
+      return people;
+    };
 
-  createLinks();
 
-  $('body').on('click', '.close', closeModal);
-  $('body').on('click', '.find_matches', findMatches);
 
-  window.HSMatch = {};
-  window.HSMatch.data = scrape();
+    var Modal = function(page) {
+      this.id = 'hsmatch-modal';
+      this.page = page;
 
-  $.post(baseUrl+'update', { data: HSMatch.data });
-}});
+      this.styles = {
+        background: '#fff',
+        border: '5px solid #000',
+        width: '920px',
+        height: '500px',
+        position: 'fixed',
+        top: '50%',
+        marginTop: '-250px',
+        left: '50%',
+        marginLeft: '-485px',
+        padding: '0 20px',
+        boxShadow: '0 0 15px rgba(0, 0, 0, .5)'
+      };
+    };
+
+    Modal.prototype.show = function(name) {
+      var html = '<div id="'+this.id+'"><h1>Hacker School Match</h1><br><ul class="matches" style="text-align:center"><p class="loading">Loading matches for '+name+' ...</p></ul><p><a href="#" class="close">Close</a></div>';
+      $(html).css(this.styles).appendTo('body');
+      this.page.getMatches(name);
+    };
+
+    Modal.prototype.closeHandler = function() {
+      var id = this.id;
+      return function(e) {
+        $('#'+id).remove();
+        e.preventDefault();
+      }
+    };
+
+    window.hsmatch = new HSMatch();
+
+    $(function() { hsmatch.init() });
+ }
+})();
